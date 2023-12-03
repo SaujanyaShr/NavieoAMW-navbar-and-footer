06895069e883c0bada95e316ace8139e4fa2a281
SaujanyaShr <152296301+SaujanyaShr@users.noreply.github.com>
2023-12-03 23:36:31 +0530

    Add files via upload

---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <h1>Employee details</h1>
+                <div>
+                    <ul id='navbar'>
+                        <li>Icon1</li>
+                        <li>Icon2</li>
+                        <li>Icon3</li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
